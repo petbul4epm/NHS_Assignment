22f817c3b762acce480c60a8b207876cf7dbc7f8
petbul4epm <petbul4e.pm@gmail.com>
2026-07-03 19:48:10 +0100

    Update Script_01.sql

--- Script_01.sql
+++ Script_01.sql
@@ -1,19 +1,19 @@
 -- CLINICS
-INSERT INTO Clinic (ClinicName, ClinicAddress)
+INSERT INTO clinic (ClinicName, ClinicAddress)
 VALUES
 ('Clinic A','10 Main Street'),
 ('Clinic B','22 River Road'),
 ('Clinic C','45 King Street');
 
 -- DOCTORS
-INSERT INTO Doctor (DoctorName, Specialty, ClinicID)
+INSERT INTO doctor (DoctorName, Specialty, ClinicID)
 VALUES
 ('Dr Adams','Cardiology',1),
 ('Dr Brown','General Practice',2),
 ('Dr Wilson','Neurology',3);
 
 -- PATIENTS
-INSERT INTO Patient
+INSERT INTO patient
 (PatientName, Address, DateOfBirth, Phone, Email)
 VALUES
 ('John Smith','123 Hill Road','1985-05-12','07111111111','john@email.com'),
@@ -21,7 +21,7 @@ VALUES
 ('David Green','789 Park Lane','1978-02-15','07333333333','david@email.com');
 
 -- MEDICATIONS
-INSERT INTO Medication
+INSERT INTO medication
 (MedicationName, Dosage)
 VALUES
 ('Aspirin','100mg'),
@@ -29,7 +29,7 @@ VALUES
 ('Beta Blocker','50mg');
 
 -- APPOINTMENTS
-INSERT INTO Appointment
+INSERT INTO appointment
 (PatientID, DoctorID, ClinicID,
 AppointmentDate, AppointmentTime, Notes)
 VALUES
@@ -37,9 +37,10 @@ VALUES
 (2,2,2,'2026-06-02','11:00:00','First Visit'),
 (3,3,3,'2026-06-03','09:30:00','Review');
 
--- PATIENT MEDICATIONS
-INSERT INTO PatientMedication
-(PatientID, MedicationID, StartDate)
+
+--TRTEATMENT
+INSERT INTO treatment
+(PatientID, TreatmentID, TreatmentName StartDate)
 VALUES
 (1,1,'2026-01-01'),
 (1,3,'2026-02-01'),
